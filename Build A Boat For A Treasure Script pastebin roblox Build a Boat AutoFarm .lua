cb51668ee464abf450bbf0e9b5ee89d9bc098223
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:52:07 +0100

    commit zero

---
+++ Build A Boat For A Treasure Script pastebin roblox Build a Boat AutoFarm .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Toxic-Mods/BuildABoat/main/BuildABoatFarm.lua'), true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Build a Boat For Treasure Scripts, AutoFarm: Start, Stop. Р•nd AntiAfk
+						
+]]
